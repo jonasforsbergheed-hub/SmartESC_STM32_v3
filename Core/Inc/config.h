88f2f98ec1588e8c9738ce5f37be159012ac3750
jonasforsbergheed-hub <jonasforsbergheed@live.se>
2026-09-15 10:24:01 +0200

    Update config.h48veway500-kugo

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -11,7 +11,7 @@
 
 //Dangerzone, do not touch!!
 #define DISPLAY_TYPE_M365DASHBOARD (1<<1)
-#define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
+#define DISPLAY_TYPE_DEBUG (1<<0)
 
 #define TRIGGER_OFFSET_ADC 50
 #define TRIGGER_DEFAULT 2020
@@ -27,8 +27,8 @@
 #define DISPLAY_TYPE DISPLAY_TYPE_M365DASHBOARD
 
 // calibration factors for voltage and current
-#define CAL_BAT_V 14 	// ADC counts * CAL_BAT_V = Battery voltage in mV
-#define CAL_I 38		// ADC counts * CAL_I = current in mA
+#define CAL_BAT_V 14
+#define CAL_I 38
 
 // gains for PI controls
 #define P_FACTOR_I_Q 100
@@ -43,40 +43,39 @@
 #define BRAKEMAX 190
 
 // parameters for speed calculation
-#define WHEEL_CIRCUMFERENCE 550 //690 for original M365 motor
-#define GEAR_RATIO 11 //15 for original M365 motor
+#define WHEEL_CIRCUMFERENCE 690
+#define GEAR_RATIO 15
 
-// speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+// speed limits for individual modes in kph
+#define SPEEDLIMIT_ECO 21
+#define SPEEDLIMIT_NORMAL 31
+#define SPEEDLIMIT_SPORT 51
 
-// motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
+// motor current limits in mA
 #define PH_CURRENT_MAX_ECO 16000
 #define PH_CURRENT_MAX_NORMAL 28000
-#define PH_CURRENT_MAX_SPORT 55000
+#define PH_CURRENT_MAX_SPORT 40000
 
 // motor current limit for regen in mA
-#define REGEN_CURRENT 20000
+#define REGEN_CURRENT 3000
 
 // maximum current for flux weakening in mA
-#define FW_CURRENT_MAX 18000 //max id
+#define FW_CURRENT_MAX 8000
 
 // maximum battery currents in mA
-#define BATTERYCURRENT_MAX 14500
-#define REGEN_CURRENT_MAX 10000
+#define BATTERYCURRENT_MAX 15000
+#define REGEN_CURRENT_MAX 3000
 
 // battery voltage limits in mV
-#define BATTERYVOLTAGE_MIN 33000
-#define BATTERYVOLTAGE_MAX 42000
-
+#define BATTERYVOLTAGE_MIN 42000
+#define BATTERYVOLTAGE_MAX 54600
 
 // motor spinning direction
-#define REVERSE 1 //1 for original M365 motor
+#define REVERSE 1
 
-// settings for speed PLL (angle estimation)
-#define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 10 //7 for original M365 motor
-#define I_FACTOR_PLL 10 //7 for original M365 motor
+// settings for speed PLL
+#define SPEED_PLL 1
+#define P_FACTOR_PLL 10
+#define I_FACTOR_PLL 10
 
 #endif /* CONFIG_H_ */
