55517fd8820d4637fc1b1a9168b2291f996bed6f
jonasforsbergheed-hub <jonasforsbergheed@live.se>
2026-09-18 18:53:50 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -1,44 +1,44 @@
-#define GASSPJÄLLFÖRSKJUTNING 45
-#define GASMAX 175
-#define BROMSFÖRSKJUTELSE 50
+#define THROTTLEOFFSET 45
+#define THROTTLEMAX 175
+#define BRAKEOFFSET 50
 #define BRAKEMAX 100
 
-// hastighetsbegränsningar
+// speed limits
 #define SPEEDLIMIT_ECO 20
-#define HASTIGHETSLIMIT_NORMAL 35
-#define HASTIGHETSLIMIT_SPORT 55
+#define SPEEDLIMIT_NORMAL 35
+#define SPEEDLIMIT_SPORT 55
 
-// motorströmsgränser
+// motor current limits
 #define PH_CURRENT_MAX_ECO 15000
 #define PH_CURRENT_MAX_NORMAL 25000
 #define PH_CURRENT_MAX_SPORT 35000
 
-// regenerera
+// regen
 #define REGEN_MAX_CURRENT 10000
 
-// fältförsvagning
-#define FÄLT_FÖRSVAGNING_STRÖM_MAX 0 //max-id
+// field weakening
+#define FIELD_WEAKNING_CURRENT_MAX 0 //max-id
 
-// ADC-kanaler
-#define ADC_SPÄNNING 0
+// ADC channels
+#define ADC_VOLTAGE 0
 #define ADC_THROTTLE 1
 #define ADC_TEMP 2
 
-#define LED-stift GPIO_PIN_1
+#define LED_Pin GPIO_PIN_1
 #define LED_GPIO_Port GPIOC
 #define UART1_Tx_Pin GPIO_PIN_6
 #define UART1_Tx_GPIO_Port GPIOB
-#define Bromsljusstift GPIO_PIN_15
-#define Bromsljus_GPIO_Port GPIOA
+#define BrakeLight_Pin GPIO_PIN_15
+#define BrakeLight_GPIO_Port GPIOA
 #define Temp_Pin GPIO_PIN_0
 #define Temp_GPIO_Port GPIOA
-#define Gasspjällspinne GPIO_PIN_1
-#define Gasspjäll_GPIO_Port GPIOA
-#define Batterispänningsstift GPIO_PIN_2
-#define Batterispänning_GPIO_Port GPIOA
-#define PWR_BTN_PIN GPIO_PIN_14
+#define Throttle_Pin GPIO_PIN_1
+#define Throttle_GPIO_Port GPIOA
+#define Batt_Voltage_Pin GPIO_PIN_2
+#define Batt_Voltage_GPIO_Port GPIOA
+#define PWR_BTN_Pin GPIO_PIN_14
 #define PWR_BTN_GPIO_Port GPIOC
-#define TPS_ENA_PIN GPIO_PIN_15
+#define TPS_ENA_Pin GPIO_PIN_15
 #define TPS_ENA_GPIO_Port GPIOC
 
 #define BATTERINIVÅ_1 323000
@@ -55,4 +55,5 @@
 #define HASTIGHET_PLL
 #define P_FACTOR_PLL 9
 #define I_FACTOR_PLL 10
+
 #define CAL_BAT_V 14
