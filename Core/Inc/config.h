092c88cfd7c2f363b300b6980346c2f9fb0b5370
jonasforsbergheed-hub <jonasforsbergheed@live.se>
2026-09-18 10:53:51 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -1,44 +1,54 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+#include "stdint.h"
+
+#define DISPLAY_TYPE_M365DASHBOARD (1<<1)
+#define DISPLAY_TYPE_DEBUG (1<<0)
+
+#define TRIGGER_OFFSET_ADC 50
+#define TRIGGER_DEFAULT 2020
+#define _T 2028
+#define SPEEDFILTER 3
+
+#define DISPLAY_TYPE DISPLAY_TYPE_M365DASHBOARD
+
+#define CAL_BAT_V 14
+#define CAL_I 38
+
+#define P_FACTOR_I_Q 100
+#define I_FACTOR_I_Q 2
+#define P_FACTOR_I_D 100
+#define I_FACTOR_I_D 10
+
 #define THROTTLEOFFSET 45
 #define THROTTLEMAX 175
 #define BRAKEOFFSET 50
-#define BRAKEMAX 100
+#define BRAKEMAX 190
 
-// speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
+#define WHEEL_CIRCUMFERENCE 2302
+#define GEAR_RATIO 1
+
+#define SPEEDLIMIT_ECO 20
+#define SPEEDLIMIT_NORMAL 35
 #define SPEEDLIMIT_SPORT 50
 
-// motor current limits for invividual modes in mA
-// note that hacked firmware allows up to 55amps motor phase current
 #define PH_CURRENT_MAX_ECO 10000
 #define PH_CURRENT_MAX_NORMAL 15000
 #define PH_CURRENT_MAX_SPORT 25000
 
-// motor current limit for regen in mA
-#define REGEN_MAX_CURRENT 10000
+#define REGEN_CURRENT 10000
+#define FW_CURRENT_MAX 0
 
-// maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define BATTERYCURRENT_MAX 20000
+#define REGEN_CURRENT_MAX 1000
 
-// ADC channels
-#define ADC_VOLTAGE 0
-#define ADC_THROTTLE 1
-#define ADC_TEMP 2
+#define BATTERYVOLTAGE_MIN 33000
+#define BATTERYVOLTAGE_MAX 54600
 
-#define LED_Pin GPIO_PIN_1
-#define LED_GPIO_Port GPIOD
-#define UART1_Tx_Pin GPIO_PIN_6
-#define UART1_Tx_GPIO_Port GPIOB
-#define BrakeLight_Pin GPIO_PIN_15
-#define BrakeLight_GPIO_Port GPIOA
-#define Temp_Pin GPIO_PIN_0
-#define Temp_GPIO_Port GPIOA
-#define Throttle_Pin GPIO_PIN_1
-#define Throttle_GPIO_Port GPIOA
-#define Batt_Voltage_Pin GPIO_PIN_2
-#define Batt_Voltage_GPIO_Port GPIOA
-#define PWR_BTN_Pin GPIO_PIN_14
-#define PWR_BTN_GPIO_Port GPIOC
-#define TPS_ENA_Pin GPIO_PIN_15
-#define TPS_ENA_GPIO_Port GPIOC
+#define REVERSE -1
 
+#define SPEED_PLL 0
+#define P_FACTOR_PLL 10
+#define I_FACTOR_PLL 10
+
+#endif
