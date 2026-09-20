6e0035bd3c1920e413325a3c932d877d612dbc81
jonasforsbergheed-hub <jonasforsbergheed@live.se>
2026-09-20 03:26:40 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -1,7 +1,7 @@
 /*
  * config.h
  *
- *  Automatically created by Lishui Parameter Configurator
+ *  Automatically SmartESC_STM32_v3 som ZIP här. by Lishui Parameter Configurator
  *  Author: stancecoke
  */
 
@@ -11,72 +11,71 @@
 
 //Dangerzone, do not touch!!
 #define DISPLAY_TYPE_M365DASHBOARD (1<<1)
-#define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
+#define DISPLAY_TYPE_DEBUG (1<<0)
 
+// Throttle / input
 #define TRIGGER_OFFSET_ADC 50
 #define TRIGGER_DEFAULT 2020
 #define _T 2028
-
 #define SPEEDFILTER 3
 
 //#define ADCTHROTTLE
 //#define FAST_LOOP_LOG
 //#define DISABLE_DYNAMIC_ADC
 
-// choose your display here
+// Display
 #define DISPLAY_TYPE DISPLAY_TYPE_M365DASHBOARD
 
-// calibration factors for voltage and current
-#define CAL_BAT_V 14 	// ADC counts * CAL_BAT_V = Battery voltage in mV
-#define CAL_I 38		// ADC counts * CAL_I = current in mA
+// Calibration factors for voltage and current
+#define CAL_BAT_V 14
+#define CAL_I 38
 
-// gains for PI controls
+// PI controls
 #define P_FACTOR_I_Q 100
 #define I_FACTOR_I_Q 2
 #define P_FACTOR_I_D 100
 #define I_FACTOR_I_D 10
 
-// min and max values of throttle and brake signals in ADC counts
+// Throttle and brake
 #define THROTTLEOFFSET 45
 #define THROTTLEMAX 175
 #define BRAKEOFFSET 50
 #define BRAKEMAX 190
 
-// parameters for speed calculation
-#define WHEEL_CIRCUMFERENCE 2302 //690 for original M365 motor
-#define GEAR_RATIO 45 //15 for original M365 motor
+// Speed calculation
+#define WHEEL_CIRCUMFERENCE 2302
+#define GEAR_RATIO 45
 
-// speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 60
-#define SPEEDLIMIT_NORMAL 200
-#define SPEEDLIMIT_SPORT 500
+// Speed limits
+#define SPEEDLIMIT_ECO 20
+#define SPEEDLIMIT_NORMAL 35
+#define SPEEDLIMIT_SPORT 55
 
-// motor current limits for invividual modes in mA
-#define PH_CURRENT_MAX_ECO 25000
+// Motor current limits
+#define PH_CURRENT_MAX_ECO 15000
 #define PH_CURRENT_MAX_NORMAL 25000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_SPORT 35000
 
-// motor current limit for regen in mA
-#define REGEN_CURRENT 20000
+// Regen
+#define REGEN_CURRENT 1000
+#define REGEN_CURRENT_MAX 10000
 
-// maximum current for flux weakening in mA
-#define FW_CURRENT_MAX 20000 //max id
+// Field weakening
+#define FW_CURRENT_MAX 0
 
-// maximum battery currents in mA
-#define BATTERYCURRENT_MAX 20000
-#define REGEN_CURRENT_MAX 1000
+// Maximum battery current
+#define BATTERYCURRENT_MAX 35000
 
-// battery voltage limits in mV
+// Battery voltage limits
 #define BATTERYVOLTAGE_MIN 33000
-#define BATTERYVOLTAGE_MAX 42000
+#define BATTERYVOLTAGE_MAX 54600
 
+// Motor spinning direction
+#define REVERSE -1
 
-// motor spinning direction
-#define REVERSE -1 //1 for original M365 motor
-
-// settings for speed PLL (angle estimation)
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 10 //7 for original M365 motor
-#define I_FACTOR_PLL 10 //7 for original M365 motor
+// Speed PLL
+#define SPEED_PLL 0
+#define P_FACTOR_PLL 10
+#define I_FACTOR_PLL 10
 
 #endif /* CONFIG_H_ */
